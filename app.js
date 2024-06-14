d4369e41cc70dc6041a5d71bbd38af418d10152d
bilalashrafali123 <ashrafalibilal385@gmail.com>
2024-06-14 15:49:14 +0500

    done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
+const card = document.querySelector('#card')
+
+console.log(form.value);
 
 
-// axios(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value}?key=YOUR_API_KEY`)
-
-
-form.addEventListener('submit' , (Event)=>{
-    Event.preventDefault()
-})
+// form.addEventListener('submit' , (Event)=>{
+//     Event.preventDefault()
+    
+// })
